10b2b60f1c57d5d06f2b2625f697c04540d6971a
sean <skelly7681@gmail.com>
2021-11-03 12:03:22 -0400

    Interfaces done

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
@@ -0,0 +1,8 @@
+package com.techelevator.tenmo.dao;
+
+public class JdbcAccountDao {
+
+
+
+
+}
---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
@@ -0,0 +1,14 @@
+package com.techelevator.tenmo.dao;
+
+import com.techelevator.tenmo.model.Transfer;
+
+import java.util.List;
+
+public interface TransferDao {
+
+    List<Transfer> getTransfer(int userId);
+    List<Transfer> getAllTransfer();
+    List<Transfer> getPendingTransfer(int userId);
+    Transfer getTransferByTransferId(int transferId);
+    
+}
